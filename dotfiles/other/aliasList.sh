651d31c169d3712bc32750cda00e4a5e6b43d9d1
pgosar <pgosarsteam@gmail.com>
2023-07-23 01:07:51 -0500

    updates

--- dotfiles/other/aliasList.sh
+++ dotfiles/other/aliasList.sh
@@ -2,44 +2,45 @@
 
 # random
 alias \
-ls="exa -F --color=auto --group-directories-first" \
-sl="exa -F --color=auto --group-directories-first" \
-lsa="exa -a" \
-grep="grep --color=auto" \
-py="bpython" \
-cd="cd -P" \
-v="nvim" \
-ps="procs"
+    ls="exa -F --color=auto --group-directories-first" \
+    sl="exa -F --color=auto --group-directories-first" \
+    lsa="exa -a" \
+    grep="grep --color=auto" \
+    py="bpython" \
+    cd="cd -P" \
+    v="nvim" \
+    ps="procs"
 
 # git stuff
 alias \
-gcom="git commit -a" \
-log="git log --graph --date=relative --pretty=tformat:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%an %ad)%Creset'" \
-gst="git status" \
-amend="git commit --amend --no-edit" \
-reword="git commit --amend" \
-untrack="git rm --cache" \
-uncommit="git reset --soft HEAD^"
+    gcom="git commit -a" \
+    log="git log --graph --date=relative --pretty=tformat:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%an %ad)%Creset'" \
+    gst="git status" \
+    amend="git commit --amend --no-edit" \
+    reword="git commit --amend" \
+    untrack="git rm --cache" \
+    uncommit="git reset --soft HEAD^" \
+    root="cd $(git rev-parse --show-cdup)"
 
 # verbosity and prompting
 alias \
-mkdir="mkdir -pv" \
-rm="rip" \
-cp="cp -iv" \
-mv="mv -iv"
+    mkdir="mkdir -pv" \
+    rm="rip" \
+    cp="cp -iv" \
+    mv="mv -iv"
 
 # packages
 alias \
-hh="hstr" \
-yt="youtube-dl --add-metadata -ic" \
-fd="~/.cargo/bin/fd" \
-mux="tmuxinator" \
-ling="github-linguist --breakdown" \
-mlp="mlp --no-browser" \
-icat="kitty +kitten icat" \
-hg="kitty +kitten hyperlinked_grep" \
-kd="kitty +kitten diff" \
-save="kitty @ ls > ~/.local/kitty.app/kitty-dump.json; cat ~/.local/kitty.app/kitty-dump.json |
+    hh="hstr" \
+    yt="youtube-dl --add-metadata -ic" \
+    fd="~/.cargo/bin/fd" \
+    mux="tmuxinator" \
+    ling="github-linguist --breakdown" \
+    mlp="mlp --no-browser" \
+    icat="kitty +kitten icat" \
+    hg="kitty +kitten hyperlinked_grep" \
+    kd="kitty +kitten diff" \
+    save="kitty @ ls > ~/.local/kitty.app/kitty-dump.json; cat ~/.local/kitty.app/kitty-dump.json |
 python3 ~/.local/kitty.app/kitty-convert-dump.py > ~/.local/kitty.app/kitty-session.kitty" \
 tn="tmux new-session -s" \
 ta="tmux attach-session -t" \
