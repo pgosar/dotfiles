82d7615c9ec155ee8b3f43c768f1c3e0f7a70046
pgosar <pgosarsteam@gmail.com>
2024-05-12 14:34:46 -0500

    cleanup

--- install.sh
+++ install.sh
@@ -1,86 +1,28 @@
-# intallation file for my dotfiles. Git clone this repo and run this file to install all the dotfiles.
-# this will only rewrite these specific files instead of removing every file in the folder
+#!/bin/bash
 
-# remove files so they can be replaced
 rm -f ~/.config/ranger/rc.conf
-rm -f ~/.config/kitty/kitty.conf
-rm -f ~/.config/kitty/nord.conf
-rm -f ~/.wezterm.lua
-rm -f ~/.bashrc
+rm -rf ~/.config/kitty
 rm -f ~/.zshrc
-rm -f ~/.profile
-rm -f ~/.config/fish/config.fish
-rm -f ~/.config/fish/fish_aliases.fish
-rm -f ~/.config/fish/fish_commands.fish
-rm -f ~/.config/fish/fish_plugins.fish
-rm -f ~/.config/fish/fish_variables
-rm -f ~/.vimrc
-rm -f ~/.config/nvim/init.vim
-rm -f ~/.config/nvim/coc-settings.json
 rm -f ~/.gitconfig
-rm -f ~/.config/i3/config
-rm -f ~/.config/i3/i3blocks.conf
-rm -f ~/.config/i3/keybindings
-rm -rf ~/.config/i3/scripts/
-rm -f ~/.config/i3blocks/config
-rm -f ~/.config/i3status/config
-rm -f ~/.config/rofi/arc_dark_colors.rasi
-rm -f ~/.config/rofi/arc_dark_transparent_colors.rasi
-rm -f ~/.config/rofi/config.rasi
-rm -f ~/.config/rofi/power-profiles.rasi
-rm -f ~/.config/rofi/powermenu.rasi
-rm -f ~/.config/rofi/rofidmenu.rasi
-rm -f ~/.config/rofi/rofikeyhint.rasi
+rm -rf ~/.config/rofi
+rm -f ~/.config/dunst/dunstrc
+rm -rf ~/.config/hypr
+rm -f ~/.config/starship.toml
+rm -f ~/.config/topgrade.toml
+rm -f ~/.tmux.conf
 
-mkdir ~/.config/fish
-mkdir ~/.config/ranger
-mkdir ~/.config/kitty
-mkdir ~/.config/nvim
-mkdir ~/.config/i3
-mkdir ~/.config/i3blocks
-mkdir ~/.config/i3status
-mkdir ~/.config/rofi
+mkdir -p ~/.config/kitty
+mkdir -p ~/.config/rofi
+mkdir -p ~/.config/dunst
+mkdir -p ~/.config/hypr
 
-
-# ranger
 ln -s ~/code/dotfiles/dotfiles/config/rc.conf ~/.config/ranger/rc.conf
-
-# kitty and wezterm terminals
-ln -s ~/code/dotfiles/dotfiles/config/kitty/kitty.conf ~/.config/kitty/kitty.conf
-ln -s ~/code/dotfiles/dotfiles/config/kitty/nord.conf ~/.config/kitty/nord.conf
-ln -s ~/code/dotfiles/dotfiles/config/wezterm.lua ~/.wezterm.lua
-
-# shells
-ln -s ~/code/dotfiles/dotfiles/config/shells/bashrc ~/.bashrc
-ln -s ~/code/dotfiles/dotfiles/config/shells/zshrc ~/.zshrc
-ln -s ~/code/dotfiles/dotfiles/config/shells/profile ~/.profile
-ln -s ~/code/dotfiles/dotfiles/config/shells/fish/config.fish ~/.config/fish/config.fish
-ln -s ~/code/dotfiles/dotfiles/config/shells/fish/fish_aliases.fish ~/.config/fish/fish_aliases.fish
-ln -s ~/code/dotfiles/dotfiles/config/shells/fish/fish_commands.fish ~/.config/fish/fish_commands.fish
-ln -s ~/code/dotfiles/dotfiles/config/shells/fish/fish_plugins ~/.config/fish/fish_plugins
-ln -s ~/code/dotfiles/dotfiles/config/shells/fish/fish_variables ~/.config/fish/fish_variables
-
-# neovim and vim
-ln -s ~/code/dotfiles/dotfiles/config/vim-nvim/vimrc ~/.vimrc
-ln -s ~/code/dotfiles/dotfiles/config/vim-nvim/init.vim ~/.config/nvim/init.vim
-ln -s ~/code/dotfiles/dotfiles/config/vim-nvim/coc-settings.json ~/.config/nvim/coc-settings.json
-
-# i3
-ln -s ~/code/dotfiles/dotfiles/config/i3/i3/config ~/.config/i3/config
-ln -s ~/code/dotfiles/dotfiles/config/i3/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
-ln -s ~/code/dotfiles/dotfiles/config/i3/i3/keybindings ~/.config/i3/keybindings
-ln -s ~/code/dotfiles/dotfiles/config/i3/i3/scripts ~/.config/i3/scripts
-ln -s ~/code/dotfiles/dotfiles/config/i3/i3blocks/config ~/.config/i3blocks/config
-ln -s ~/code/dotfiles/dotfiles/config/i3/i3status/config ~/.config/i3status/config
-
-# rofi
-ln -s ~/code/dotfiles/dotfiles/config/rofi/arc_dark_colors.rasi ~/.config/rofi/arc_dark_colors.rasi
-ln -s ~/code/dotfiles/dotfiles/config/rofi/arc_dark_transparent_colors.rasi ~/.config/rofi/arc_dark_transparent_colors.rasi
-ln -s ~/code/dotfiles/dotfiles/config/rofi/config.rasi ~/.config/rofi/config.rasi
-ln -s ~/code/dotfiles/dotfiles/config/rofi/power-profiles.rasi ~/.config/rofi/power-profiles.rasi
-ln -s ~/code/dotfiles/dotfiles/config/rofi/powermenu.rasi ~/.config/rofi/powermenu.rasi
-ln -s ~/code/dotfiles/dotfiles/config/rofi/rofidmenu.rasi ~/.config/rofi/rofidmenu.rasi
-ln -s ~/code/dotfiles/dotfiles/config/rofi/rofikeyhint.rasi ~/.config/rofi/rofikeyhint.rasi
-
-# other
+ln -s ~/code/dotfiles/dotfiles/config/hypr ~/.config/hypr
+ln -s ~/code/dotfiles/dotfiles/config/kitty ~/.config/kitty/
+ln -s ~/code/dotfiles/dotfiles/config/zshrc ~/.zshrc
+ln -s ~/code/dotfiles/dotfiles/config/rofi ~/.config/rofi
 ln -s ~/code/dotfiles/dotfiles/config/gitconfig ~/.gitconfig
+ln -s ~/code/dotfiles/dotfiles/config/dunst ~/.config/dunst
+ln -s ~/code/dotfiles/dotfiles/config/starship.toml ~/.config/starship.toml
+ln -s ~/code/dotfiles/dotfiles/config/topgrade.toml ~/.config/topgrade.toml
+ln -s ~/code/dotfiles/dotfiles/config/tmux.conf ~/.tmux.conf
