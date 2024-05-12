b0bc11398c15565828d273662fbb151224099964
pgosar <pgosarsteam@gmail.com>
2024-05-12 15:13:57 -0500

    fix paths

--- install.sh
+++ install.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
-rm -f ~/.config/ranger/rc.conf
-rm -rf ~/.config/kitty
 rm -f ~/.zshrc
 rm -f ~/.gitconfig
+rm -f ~/.tmux.conf
+rm -rf ~/.config/ranger
+rm -rf ~/.config/kitty
 rm -rf ~/.config/rofi
 rm -rf ~/.config/dunst
 rm -rf ~/.config/hypr
 rm -f ~/.config/starship.toml
 rm -f ~/.config/topgrade.toml
-rm -f ~/.tmux.conf
-
-mkdir -p ~/.config/kitty
-mkdir -p ~/.config/rofi
-mkdir -p ~/.config/ranger
-mkdir -p ~/.config/dunst
-mkdir -p ~/.config/hypr
 
 source ./dotfiles_env.sh
 
-ln -s "$DOTFILES_DIR/config/rc.conf" ~/.config/ranger/rc.conf
+ln -s "$DOTFILES_DIR/config/ranger" ~/.config/ranger
 ln -s "$DOTFILES_DIR/config/hypr" ~/.config/hypr
-ln -s "$DOTFILES_DIR/config/kitty" ~/.config/kitty/
+ln -s "$DOTFILES_DIR/config/kitty" ~/.config/kitty
 ln -s "$DOTFILES_DIR/config/zshrc" ~/.zshrc
 ln -s "$DOTFILES_DIR/config/rofi" ~/.config/rofi
 ln -s "$DOTFILES_DIR/config/gitconfig" ~/.gitconfig
