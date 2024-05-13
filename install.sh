755fc4574b69e65be4433ec816e94635477e068a
pgosar <pgosarsteam@gmail.com>
2024-05-12 22:04:24 -0500

    up

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -f ~/.zshrc
+rm -f ~/.zprofile
 rm -f ~/.gitconfig
 rm -f ~/.tmux.conf
 rm -rf ~/.config/ranger
@@ -8,6 +9,7 @@ rm -rf ~/.config/kitty
 rm -rf ~/.config/rofi
 rm -rf ~/.config/dunst
 rm -rf ~/.config/hypr
+rm -rf ~/.config/waybar
 rm -f ~/.config/starship.toml
 rm -f ~/.config/topgrade.toml
 
@@ -17,9 +19,11 @@ ln -s "$DOTFILES_DIR/config/ranger" ~/.config/ranger
 ln -s "$DOTFILES_DIR/config/hypr" ~/.config/hypr
 ln -s "$DOTFILES_DIR/config/kitty" ~/.config/kitty
 ln -s "$DOTFILES_DIR/config/zshrc" ~/.zshrc
+ln -s "$DOTFILES_DIR/config/zprofile" ~/.zprofile
 ln -s "$DOTFILES_DIR/config/rofi" ~/.config/rofi
 ln -s "$DOTFILES_DIR/config/gitconfig" ~/.gitconfig
 ln -s "$DOTFILES_DIR/config/dunst" ~/.config/dunst
+ln -s "$DOTFILES_DIR/config/waybar" ~/.config/waybar
 ln -s "$DOTFILES_DIR/config/starship.toml" ~/.config/starship.toml
 ln -s "$DOTFILES_DIR/config/topgrade.toml" ~/.config/topgrade.toml
 ln -s "$DOTFILES_DIR/config/tmux.conf" ~/.tmux.conf
