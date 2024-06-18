8fc8d54894ba60b3f2a3303f77cb00ec4c59179b
pgosar <pgosarsteam@gmail.com>
2024-06-18 17:49:20 -0500

    switching to macos

--- install.sh
+++ install.sh
@@ -6,10 +6,6 @@ rm -f ~/.gitconfig
 rm -f ~/.tmux.conf
 rm -rf ~/.config/ranger
 rm -rf ~/.config/kitty
-rm -rf ~/.config/rofi
-rm -rf ~/.config/dunst
-rm -rf ~/.config/hypr
-rm -rf ~/.config/waybar
 rm -rf ~/.config/nvim
 rm -f ~/.config/starship.toml
 rm -f ~/.config/topgrade.toml
@@ -18,14 +14,10 @@ source ./dotfiles_env.sh
 
 ln -s "$DOTFILES_DIR/config/nvim" ~/.config/nvim
 ln -s "$DOTFILES_DIR/config/ranger" ~/.config/ranger
-ln -s "$DOTFILES_DIR/config/hypr" ~/.config/hypr
 ln -s "$DOTFILES_DIR/config/kitty" ~/.config/kitty
 ln -s "$DOTFILES_DIR/config/zshrc" ~/.zshrc
 ln -s "$DOTFILES_DIR/config/zprofile" ~/.zprofile
-ln -s "$DOTFILES_DIR/config/rofi" ~/.config/rofi
 ln -s "$DOTFILES_DIR/config/gitconfig" ~/.gitconfig
-ln -s "$DOTFILES_DIR/config/dunst" ~/.config/dunst
-ln -s "$DOTFILES_DIR/config/waybar" ~/.config/waybar
 ln -s "$DOTFILES_DIR/config/starship.toml" ~/.config/starship.toml
 ln -s "$DOTFILES_DIR/config/topgrade.toml" ~/.config/topgrade.toml
 ln -s "$DOTFILES_DIR/config/tmux.conf" ~/.tmux.conf
