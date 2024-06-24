863e905135165723e701e10bc80a5cd89c507b49
pgosar <pgosarsteam@gmail.com>
2024-06-23 22:07:49 -0500

    clean up aliases

--- config/aliasList.sh
+++ config/aliasList.sh
@@ -10,8 +10,7 @@ alias \
 	cd="cd -P" \
 	v="nvim" \
 	ps="procs" \
-	:q="exit" \
-	rsync="rsync -avh --progress"
+	:q="exit"
 
 # git stuff
 alias \
@@ -33,30 +32,12 @@ alias \
 
 # packages
 alias \
-	hh="hstr" \
 	yt="youtube-dl --add-metadata -ic" \
 	fd="~/.cargo/bin/fd" \
 	mlp="mlp --no-browser" \
 	icat="kitty +kitten icat" \
 	hg="kitty +kitten hyperlinked_grep" \
-	kd="kitty +kitten diff" \
-	save="kitty @ ls > ~/.local/kitty.app/kitty-dump.json; cat ~/.local/kitty.app/kitty-dump.json |
-python3 ~/.local/kitty.app/kitty-convert-dump.py > ~/.local/kitty.app/kitty-session.kitty" \
-	wiki="wikiSummary.py" \
-	lock="swaylock \
-        --screenshots \
-        --clock \
-        --indicator \
-        --indicator-radius 100 \
-        --indicator-thickness 7 \
-        --effect-blur 7x5 \
-        --effect-vignette 0.5:0.5 \
-        --ring-color bb00cc \
-        --key-hl-color 880033 \
-        --line-color 00000000 \
-        --inside-color 00000088 \
-        --separator-color 00000000 \
-        --fade-in 0.2"
+	kd="kitty +kitten diff"
 
 # other
 eval "$(zoxide init zsh)"
