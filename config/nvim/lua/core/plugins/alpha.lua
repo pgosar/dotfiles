c0ac7f7a3236fbb225f5290b0ff1580dc8cbf126
pgosar <pgosarsteam@gmail.com>
2024-06-29 23:32:09 -0500

    fix dashboard icon

--- config/nvim/lua/core/plugins/alpha.lua
+++ config/nvim/lua/core/plugins/alpha.lua
@@ -16,7 +16,7 @@ return {
 			dashboard.button("e", "  > New file", ":lua require('core.utils.utils').create_new_file()<CR>"),
 			dashboard.button("f", "  > Find file in git repo", ":Telescope git_files <CR>"),
 			dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
-			dashboard.button("l", "🗘  > Open last session", ":PossessionLoad<CR>"),
+			dashboard.button("l", "  > Open last session", ":PossessionLoad<CR>"),
 			dashboard.button("o", "  > Open session", ":Telescope possession list<CR>"),
 			dashboard.button("p", "  > Open project", ":Telescope projects<CR>"),
 		}
