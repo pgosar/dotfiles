469ca1ad7e7518f125ebbf6fb0011e342dc44f31
pgosar <pgosarsteam@gmail.com>
2024-12-16 22:36:16 -0600

    switch to flash

---
+++ config/nvim/lua/core/plugins/flash.lua
@@ -0,0 +1,17 @@
+return {
+	"folke/flash.nvim",
+	cond = group.plugins.flash,
+	opts = {
+		modes = {
+			search = { enabled = true },
+			char = { jump_labels = true },
+		},
+	},
+  -- stylua: ignore
+  keys = {
+    -- make sure it's activated when searching
+    {"/", "/"},
+    {"?", "?"},
+    { "<leader>j", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "<leader>J", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" }, },
+}
--- config/nvim/lua/core/plugins/hop.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"smoka7/hop.nvim",
-	cond = group.plugins.hop,
-	cmd = "HopWord",
-	keys = {
-		{ "<leader>j", "<CMD>HopWord<CR>", desc = "hop across file" },
-	},
-	opts = { multi_windows = true },
-}
