21552e9a69cff8685054c5d8abe37b27cb15b90a
pgosar <pgosarsteam@gmail.com>
2024-12-31 16:50:49 -0600

    fix(plugins): don't enable flash when searching

--- config/nvim/lua/core/plugins/flash.lua
+++ config/nvim/lua/core/plugins/flash.lua
@@ -3,14 +3,10 @@ return {
 	cond = group.plugins.flash,
   -- stylua: ignore
   keys = {
-    -- make sure it's activated when searching
-    {"/", "/"},
-    {"?", "?"},
     { "<leader>j", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "<leader>J", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" }, },
 	opts = {
 		modes = {
-			search = { enabled = true },
 			char = { jump_labels = true },
 		},
 	},
