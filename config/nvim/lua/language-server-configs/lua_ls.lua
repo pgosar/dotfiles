99ff6afb9fb9a40860a1a26ce7902b2413cb2c15
pgosar <pgosarsteam@gmail.com>
2024-12-31 17:15:44 -0600

    fix: ensure consistent indentation style

--- config/nvim/lua/language-server-configs/lua_ls.lua
+++ config/nvim/lua/language-server-configs/lua_ls.lua
@@ -3,6 +3,12 @@ return {
 		Lua = {
 			diagnostics = { disable = { "missing-fields" } },
 			hint = { enable = true },
+			format = {
+				enable = true,
+				defaultConfig = {
+					indent_style = "tab",
+				},
+			},
 		},
 	},
 }
