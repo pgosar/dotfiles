7fc4092592c3210f140d58204364d5766014f2f6
pgosar <pgosarsteam@gmail.com>
2025-01-30 21:05:23 -0600

    fix(plugins): fix deprecated config spec

--- config/nvim/lua/core/plugins/markview.lua
+++ config/nvim/lua/core/plugins/markview.lua
@@ -6,16 +6,18 @@ return {
 		{ "<leader>mm", "<CMD>Markview toggle<CR>", desc = "Toggle markdown viewer" },
 	},
 	opts = {
-		modes = { "n", "i", "no", "c" },
-		hybrid_modes = { "i" },
+		preview = {
+			modes = { "n", "i", "no", "c" },
+			hybrid_modes = { "i" },
 
-		callbacks = {
-			on_enable = function(_, win)
-				vim.wo[win].conceallevel = 2
-				vim.wo[win].concealcursor = "nc"
-			end,
+			callbacks = {
+				on_enable = function(_, win)
+					vim.wo[win].conceallevel = 2
+					vim.wo[win].concealcursor = "nc"
+				end,
+			},
+			filetypes = { "markdown", "quarto", "rmd", "tex" },
 		},
-		filetypes = { "markdown", "quarto", "rmd", "tex" },
 		html = { tags = { default = { conceal = true } } },
 	},
 }
