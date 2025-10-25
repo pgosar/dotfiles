d4d7b3cfdfe3e13317a1d29dd46a026f1c73b7da
pgosar <pgosarsteam@gmail.com>
2025-10-25 11:03:22 -0700

    fix(lsp): update changed rust analyzer opts

--- config/nvim/lua/language-server-configs/rust-analyzer.lua
+++ config/nvim/lua/language-server-configs/rust-analyzer.lua
@@ -3,7 +3,8 @@ return {
 		["rust-analyzer"] = {
 			inlayHints = { genericParameterHints = { lifetime = { enable = true } } },
 			diagnostics = { styleLints = { enable = true } },
-			checkOnSave = {
+      checkOnSave = true,
+			check = {
 				allFeatures = true,
 				command = "clippy",
 			},
