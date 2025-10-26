39310ea2f04d3b8f3523953e602a2374d4e3b8dc
pgosar <pgosarsteam@gmail.com>
2025-10-25 20:50:44 -0700

    fix(plugins): remove unnecessary stylua ignore

--- config/nvim/lua/core/plugins/flash.lua
+++ config/nvim/lua/core/plugins/flash.lua
@@ -1,7 +1,6 @@
 return {
   "folke/flash.nvim",
   cond = group.plugins.flash,
-  -- stylua: ignore
   keys = {
     { "<leader>j", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
     { "<leader>J", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" }, },
