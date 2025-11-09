7cd2b9f8d887b122066ca69272614c23b6bffa3c
pgosar <pgosarsteam@gmail.com>
2025-11-08 16:09:09 -0800

    feat(plugins): switch to snacks zen from zenmode

--- config/nvim/lua/core/plugins/snacks.lua
+++ config/nvim/lua/core/plugins/snacks.lua
@@ -1,8 +1,28 @@
 return {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  opts = {
-    picker = { enabled = group.plugins.snacks_picker },
-  },
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		picker = { enabled = group.plugins.snacks_picker },
+		zen = {
+			enabled = true,
+			win = {
+				backdrop = { transparent = false },
+				wo = { winbar = "" },
+			},
+			toggles = { dim = false },
+			on_open = function()
+				require("dropbar.menu").dropbar_menu_t:del()
+			end,
+		},
+	},
+	keys = {
+		{
+			"<leader>zm",
+			function()
+				Snacks.zen()
+			end,
+			desc = "Toggle Zen Mode",
+		},
+	},
 }
