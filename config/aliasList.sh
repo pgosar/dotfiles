9122787509318da2f3d5cea93a821dfa74882e91
pgosar <pgosarsteam@gmail.com>
2026-02-27 23:07:32 -0800

    feat(alias): add icons to ls aliases

--- config/aliasList.sh
+++ config/aliasList.sh
@@ -4,32 +4,32 @@ OS="$(uname)"
 
 # random
 alias \
-       ls="eza -F --color=auto --group-directories-first" \
-       sl="eza -F --color=auto --group-directories-first" \
-       lsa="eza -F --color=auto --group-directories-first -a" \
-       grep="grep --color=auto" \
-       py="bpython" \
-       cd="cd -P" \
-       v="nvim" \
-       ps="procs" \
-       :q="exit"
+  ls="eza -F --icons --color=auto --group-directories-first" \
+  sl="eza -F --icons --color=auto --group-directories-first" \
+  lsa="eza -F --icons --color=auto --group-directories-first -a" \
+  grep="grep --color=auto" \
+  py="bpython" \
+  cd="cd -P" \
+  v="nvim" \
+  ps="procs" \
+  :q="exit"
 
 # git stuff
 alias \
-       gcom="git commit -a" \
-       log="git log --graph --date=relative --pretty=tformat:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%an %ad)%Creset'" \
-       gst="git status" \
-       amend="git commit --amend --no-edit" \
-       reword="git commit --amend" \
-       untrack="git rm --cache" \
-       uncommit="git reset --soft HEAD^" \
-       groot='cd $(git rev-parse --show-toplevel 2>/dev/null)'
+  gcom="git commit -a" \
+  log="git log --graph --date=relative --pretty=tformat:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%an %ad)%Creset'" \
+  gst="git status" \
+  amend="git commit --amend --no-edit" \
+  reword="git commit --amend" \
+  untrack="git rm --cache" \
+  uncommit="git reset --soft HEAD^" \
+  groot='cd $(git rev-parse --show-toplevel 2>/dev/null)'
 
 # verbosity and prompting
 alias \
-       mkdir="mkdir -pv" \
-       cp="cp -iv" \
-       mv="mv -iv"
+  mkdir="mkdir -pv" \
+  cp="cp -iv" \
+  mv="mv -iv"
 
 if [ "$OS" = "Linux" ]; then
   alias rm="rip"
@@ -39,20 +39,20 @@ fi
 
 # packages
 alias \
-       mlp="mlp --no-browser" \
-       icat="kitty +kitten icat" \
-       hg="kitty +kitten hyperlinked_grep" \
-       kd="kitty +kitten diff"
+  mlp="mlp --no-browser" \
+  icat="kitty +kitten icat" \
+  hg="kitty +kitten hyperlinked_grep" \
+  kd="kitty +kitten diff"
 
 if [ "$OS" = "Linux" ]; then
   alias \
-         hh="hstr" \
-         yt="youtube-dl --add-metadata -ic" \
-         fd="~/.cargo/bin/fd" \
-         save="kitty @ ls > ~/.local/kitty.app/kitty-dump.json; cat ~/.local/kitty.app/kitty-dump.json |
+    hh="hstr" \
+    yt="youtube-dl --add-metadata -ic" \
+    fd="~/.cargo/bin/fd" \
+    save="kitty @ ls > ~/.local/kitty.app/kitty-dump.json; cat ~/.local/kitty.app/kitty-dump.json |
 python3 ~/.local/kitty.app/kitty-convert-dump.py > ~/.local/kitty.app/kitty-session.kitty" \
-         wiki="wikiSummary.py" \
-         lock="swaylock \
+    wiki="wikiSummary.py" \
+    lock="swaylock \
          --screenshots \
          --clock \
          --indicator \
