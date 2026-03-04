7d3205dadd49d62d6989794d31fcad0a85a76fcb
pgosar <pgosarsteam@gmail.com>
2026-03-03 22:30:16 -0800

    feat(ranger): delete ranger (switching to yazi)

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ rm -f ~/.zshrc
 rm -f ~/.zprofile
 rm -f ~/.gitconfig
 rm -f ~/.tmux.conf
-rm -rf ~/.config/ranger
 rm -rf ~/.config/kitty
 rm -rf ~/.config/nvim
 rm -f ~/.config/spicetify/config-xpui.ini
@@ -42,7 +41,6 @@ source ./dotfiles_env.sh
 # ---- Shared Symlinks -------------------------------------------------------
 
 ln -s "$DOTFILES_DIR/config/nvim" ~/.config/nvim
-ln -s "$DOTFILES_DIR/config/ranger" ~/.config/ranger
 ln -s "$DOTFILES_DIR/config/kitty" ~/.config/kitty
 ln -s "$DOTFILES_DIR/config/zshrc" ~/.zshrc
 ln -s "$DOTFILES_DIR/config/zprofile" ~/.zprofile
