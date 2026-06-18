6572f341abb5df000398795fb14ddda04df2128d
pgosar <pgosarsteam@gmail.com>
2026-06-17 20:57:52 -0700

    fix(systemd): don't bother compressing over network

--- home/scripts/minecraft_backup.sh
+++ home/scripts/minecraft_backup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -avz -e ssh --delete ~/.local/share/gdlauncher_carbon/data/instances/ chilly@192.168.1.184:~/Replicas/Minecraft
+rsync -av -e ssh --delete ~/.local/share/gdlauncher_carbon/data/instances/ chilly@192.168.1.184:~/Replicas/Minecraft/
